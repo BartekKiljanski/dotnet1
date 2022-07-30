91d402ddef15e0bf1c4d5705793c01b4a5a0409f
Bartłomiej Kiljański <bartek240397@interia.pl>
2022-07-30 12:17:26 +0200

    edycja

---
+++ Robot.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    internal class Robot
+    {
+        private string serialNumber;
+        public string Name;
+        public Robot(string serialNumber)
+        {
+            Console.WriteLine("Konstruktor");
+            this.serialNumber = serialNumber;
+        }
+        public void SayHello()
+        {
+            Console.WriteLine("Cześć, jestem robot .Mój numer seryjny to:" + serialNumber );
+        }
+        public DateTime GetCurrentDate()
+        {
+            var currentTime = DateTime.Now;
+            return currentTime;
+        }
+        public static void SayHelloStatic()
+        {
+            Console.WriteLine("czesc, jestem robot. Wywołałes metodę statyczna ");
+        }
+
+    }
+}
